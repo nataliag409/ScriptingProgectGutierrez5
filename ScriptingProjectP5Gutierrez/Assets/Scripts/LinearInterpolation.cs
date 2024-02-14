e675883cc5072018f7919be5ce41f6812fb7718a
nataliag409 <natalia212329@gmail.com>
2024-02-14 12:26:53 -0800

    tdyjdty6
    tdyjdty6

---
+++ ScriptingProjectP5Gutierrez/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+  * 
+  *
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = Mathf.Lerp(lighty.intensit, 8F, 0.5f);
+        light.intensity = Mathf.Lerp(light.intensity,8f, 0.5f * Time.deltaTime);
+    }
+    */
+}

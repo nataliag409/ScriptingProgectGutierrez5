3b2be2338f23917c10dc2a8977e7ee33431450b4
nataliag409 <natalia212329@gmail.com>
2024-03-12 12:36:17 -0700

    ihlik
    jhfjhbli

---
+++ ScriptingProjectP5Gutierrez/Assets/Scripts/DualAxisExample.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DualAxisExample : MonoBehaviour
+{
+    public float hRange;
+    public float vRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("vertical"); ;
+        float xPos = h * hRange;
+        float yPos = v * hRange;
+
+        transform.position = new Vector3(xPos, 0, yPos);
+
+    }
+}
